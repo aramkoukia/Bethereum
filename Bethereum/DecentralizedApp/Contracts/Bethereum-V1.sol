5b5389837546f467118cc0cc458fe8a4db6c16ed
aramkoukia <aramkoukia@gmail.com>
2017-01-17 14:20:41 -0800

    some more changes to the contract

--- Bethereum/DecentralizedApp/Contracts/Bethereum-V1.sol
+++ Bethereum/DecentralizedApp/Contracts/Bethereum-V1.sol
@@ -1,72 +1,92 @@
+pragma solidity ^0.4.0;
+
 contract RouteCoin {
-    // Parameters of the contract. Times are either
-    // absolute unix timestamps (seconds since 1970-01-01)
-    // or time periods in seconds.
-    
     // The public key of the buyer. Reza: we need to hash this.
-    address buyer;   
+    address public buyer;   
+
+    address public seller;
 
     // Q: is this a Wallet address? an IP address? 
     // The destination of RREQ
-    address finalDestination;  
+    address public finalDestination;  
 
     // The deadline when the contract will end automatically
-    uint contractStartTime;
+    uint public contractStartTime;
 
     // The duration of the contract will end automatically
-    uint contractGracePeriod;
+    uint public contractGracePeriod;
 
     // The contract prize amount. 
     // Q: will this be with Ethers or we create a coin called RouteCoin?
-    uint contractPrize;
+    uint public contractPrize;
+    
+    enum State { Created, Expired, Completed, Aborted }
+    State public state;
 
-    // Current state of the contract.
-    struct ContractState
-    {
-        // The address of the winner of the RREQ. Reza: we need to hash this
-        address winnerAddress;
-        // Stores the state of the contract. 
-        // 1:Open, 2:Expired 3:Finished. Other?
-        uint finalState;
-        // specifies that the contract is ended so no changes is done after
-        bool contractEnded;
-    } 
+    //function Purchase() payable {
+    //    seller = msg.sender;
+    //value = msg.contractPrize / 2;  // transfer half of fee
+    //if (2 * value != msg.value) throw;
+    //}
 
-    // Events that will be fired on changes.
-    //event ContractEnded(address winner, uint amount);
-
-    function RouteCoin(address _buyer, address _finalDestination, uint _contractTime, uint _contractGracePeriod, uint _contractPrize)  
-    {
-        buyer = _buyer;
-        finalDestination = _finalDestination;
-        contractStartTime = now;
-        contractGracePeriod = _contractGracePeriod;
-        contractPrize = _contractPrize;
+    modifier require(bool _condition) {
+        if (!_condition) throw;
+        _;
     }
 
-    function SendRreq() {
-        // No arguments are necessary, all information is already part of the transaction. 
-        if (now > contractStartTime + contractGracePeriod) {
-            // Revert the call if the contract grace period is over.
-            throw;
-        }
+    modifier onlyBuyer() {
+        if (msg.sender != buyer) throw;
+        _;
     }
 
-    /// End the auction and send the highest bid
-    /// to the beneficiary.
-    function ContractClosed() {
+    modifier onlySeller() {
+        if (msg.sender != seller) throw;
+        _;
+    }
 
-        if (now <= auctionStart + biddingTime)
-            throw; // auction did not yet end
-        if (ended)
-            throw; // this function has already been called
+    modifier inState(State _state) {
+        if (state != _state) throw;
+        _;
+    }
 
-        // 2. Effects
-        ContractState.contractEnd = true;
-        //ContractEnded(winner, amount);
+    event expired();
+    event aborted();
+    event routeFound();
+    event routeAccepted();
 
-        // 3. Interaction
-        if (!beneficiary.send(highestBid))
+    function abort()
+        onlyBuyer // only buyer can abort the contract
+        inState(State.Created)
+    {
+        aborted();
+        state = State.Aborted;
+        //if (!buyer.send(this.balance))
+        //    throw;
+    }
+
+    /// Confirm the purchase as buyer.
+    /// Transaction has to include `2 * value` ether.
+    /// The ether will be locked until confirmReceived
+    /// is called.
+    function foundDestinationAddress()
+        onlySeller
+        inState(State.Created)
+        payable
+    {
+        routeFound();
+        buyer = msg.sender;
+        //state = State.Locked;
+    }
+
+    /// Confirm that you (the buyer) received the item.
+    /// This will release the locked ether.
+    function confirmPurchase()
+        onlyBuyer
+        inState(State.Created)
+    {
+        routeAccepted();
+        state = State.Completed;
+        if (!buyer.send(contractPrize))
             throw;
     }
 }
